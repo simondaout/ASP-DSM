a4cf260b27f4fa10b9d035a02e18e2ac61614880
simondaout <simondaout@gmail.com>
2024-02-05 17:22:34 +0100

    pleiades correlation script

---
+++ launch_correl_pleiades.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+# load asp
+module load asp
+
+# help
+Help()
+{
+   # Display Help
+   echo "Syntax: launch_correl_pleiades.sh asp_parameters.txt"
+   echo "asp_parameters.txt: parameter file"
+   echo
+}
+
+if [ -z $1 ]; then { Help; exit 1; } fi
+source $1
+
+IMG1="$HOME/INPUT/orthoimage_nadir_$DATE1.tif"
+IMG2="$HOME/INPUT/orthoimage_nadir_$DATE2.tif"
+IMG1_CROP="$HOME/INPUT/orthoimage_nadir_crop_$DATE1.tif"
+IMG2_CROP="$HOME/INPUT/orthoimage_nadir_crop_$DATE2.tif"
+
+#####################
+# CROP
+#####################
+
+CROP1=`echo ${CROP[0]}`
+CROP2=`echo ${CROP[1]}`
+CROP3=`echo ${CROP[2]}`
+CROP4=`echo ${CROP[3]}`
+
+gdal_translate -projwin $CROP1 $CROP2 $CROP3 $CROP4  -of GTiff $IMG1 $IMG1_CROP 
+gdal_translate -projwin $CROP1 $CROP2 $CROP3 $CROP4  -of GTiff $IMG2 $IMG2_CROP  
+
+#####################
+# CORRELATION
+#####################
+
+cd $HOME
+PAIR=$DATE1"_"$DATE2
+rm -rf $PAIR
+mkdir $PAIR
+cd $PAIR
+
+session=" -t $SESSION_TYPE --individually-normalize  --alignment-method $A_M --threads-multiprocess $THREADS"
+stereo="--corr-kernel $CORR_KERNEL --cost-mode $COST_MODE --stereo-algorithm $ST_ALG --corr-tile-size $CORR_T_S --subpixel-mode $SUBP_MODE --subpixel-kernel $SUBP_KERNEL --corr-seed-mode $CORR_S_MODE --xcorr-threshold $XCORR_TH --min-xcorr-level $MIN_XCORR_LVL --sgm-collar-size $SGM_C_SIZE"
+denoising="--rm-quantile-multiple $RM_QUANT_MULT --filter-mode $FILTER_MODE" 
+filtering="--median-filter-size $MED_FILTER_SIZE --texture-smooth-size $TEXT_SMOOTH_SIZE --texture-smooth-scale $TEXT_SMOOTH_SCALE"
+
+parallel_stereo $session $IMG1_CROP $IMG2_CROP $BLACK_LEFT $BLACK_RIGHT $OUTPUT_DIR $stereo $filtering  
+gdal_translate -q -b 1 -co COMPRESS=LZW $OUTPUT_DIR-F.tif EW_2014_filter.tif
+gdal_translate -q -b 2 -co COMPRESS=LZW $OUTPUT_DIR-F.tif NS_2014_filter.tif
+gdal_translate -q -b 3 -co COMPRESS=LZW $OUTPUT_DIR-F.tif CC_2014_filter.tif
+
+DATE1="202301150511233"
+DATE2="202312190511471"
