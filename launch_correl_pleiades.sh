a45b8766c97cc6dcc817f52bc69bc1383cc083dc
simondaout <simondaout@gmail.com>
2024-02-05 18:51:04 +0100

    change output dirs

--- launch_correl_pleiades.sh
+++ launch_correl_pleiades.sh
@@ -15,10 +15,10 @@ Help()
 if [ -z $1 ]; then { Help; exit 1; } fi
 source $1
 
-IMG1="$HOME/INPUT/orthoimage_nadir_$DATE1.tif"
-IMG2="$HOME/INPUT/orthoimage_nadir_$DATE2.tif"
-IMG1_CROP="$HOME/INPUT/orthoimage_nadir_crop_$DATE1.tif"
-IMG2_CROP="$HOME/INPUT/orthoimage_nadir_crop_$DATE2.tif"
+IMG1="$HOME/INPUT/orthoimage_"$GEOM"_"$DATE1".tif"
+IMG2="$HOME/INPUT/orthoimage_"$GEOM"_"$DATE2".tif"
+IMG1_CROP="$HOME/INPUT/orthoimage_"$GEOM"_crop_"$DATE1".tif"
+IMG2_CROP="$HOME/INPUT/orthoimage_"$GEOM"_crop_"$DATE2".tif"
 
 #####################
 # CROP
@@ -37,8 +37,10 @@ gdal_translate -projwin $CROP1 $CROP2 $CROP3 $CROP4  -of GTiff $IMG2 $IMG2_CROP
 #####################
 
 cd $HOME
-PAIR=$DATE1"_"$DATE2
-rm -rf $PAIR
+DIR1=${DATE1:0:8}
+DIR2=${DATE2:0:8}
+PAIR=$DIR1"_"$DIR2
+#rm -rf $PAIR
 mkdir $PAIR
 cd $PAIR
 
@@ -48,9 +50,7 @@ denoising="--rm-quantile-multiple $RM_QUANT_MULT --filter-mode $FILTER_MODE"
 filtering="--median-filter-size $MED_FILTER_SIZE --texture-smooth-size $TEXT_SMOOTH_SIZE --texture-smooth-scale $TEXT_SMOOTH_SCALE"
 
 parallel_stereo $session $IMG1_CROP $IMG2_CROP $BLACK_LEFT $BLACK_RIGHT $OUTPUT_DIR $stereo $filtering  
-gdal_translate -q -b 1 -co COMPRESS=LZW $OUTPUT_DIR-F.tif EW_2014_filter.tif
-gdal_translate -q -b 2 -co COMPRESS=LZW $OUTPUT_DIR-F.tif NS_2014_filter.tif
-gdal_translate -q -b 3 -co COMPRESS=LZW $OUTPUT_DIR-F.tif CC_2014_filter.tif
+gdal_translate -q -b 1 -co COMPRESS=LZW $OUTPUT_DIR-F.tif EW_"$GEOM"_"${DATE1:0:8}"_"${DATE2:0:8}"_filter.tif
+gdal_translate -q -b 2 -co COMPRESS=LZW $OUTPUT_DIR-F.tif NS_"$GEOM"_"${DATE1:0:8}"_"${DATE2:0:8}"_filter.tif
+gdal_translate -q -b 3 -co COMPRESS=LZW $OUTPUT_DIR-F.tif CC_"$GEOM"_"${DATE1:0:8}"_"${DATE2:0:8}"_filter.tif
 
-DATE1="202301150511233"
-DATE2="202312190511471"
