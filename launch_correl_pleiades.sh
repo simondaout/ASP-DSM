622d2a32749611ba379a0818ef7411b00a88ca90
simondaout <simondaout@gmail.com>
2024-02-12 13:28:37 +0100

    add filtering options

--- launch_correl_pleiades.sh
+++ launch_correl_pleiades.sh
@@ -55,13 +55,22 @@ PAIR=$DIR1"_"$DIR2
 mkdir $PAIR
 cd $PAIR
 
+if [ -z "$FILTER_MODE" ]; then
+    FILTER_MODE="2"
+fi
 if [ -z "$RM_QUANT_PC" ]; then
     RM_QUANT_PC="0.85"
+fi
+if [ -z "$RM_MIN_MATCHES" ]; then
+    RM_MIN_MATCHES="60"
+fi
+if [ -z "$RM_THRESHOLD" ]; then
+    RM_THRESHOLD="3"
 fi
 
 session=" -t $SESSION_TYPE --individually-normalize --alignment-method $A_M --threads-multiprocess $THREADS --process $THREADS --no-bigtiff --tif-compress Deflate"
 stereo="--corr-kernel $CORR_KERNEL --cost-mode $COST_MODE --stereo-algorithm $ST_ALG --corr-tile-size $CORR_T_S --subpixel-mode $SUBP_MODE --subpixel-kernel $SUBP_KERNEL --corr-seed-mode $CORR_S_MODE --xcorr-threshold $XCORR_TH --min-xcorr-level $MIN_XCORR_LVL --sgm-collar-size $SGM_C_SIZE "
-denoising="--rm-quantile-multiple $RM_QUANT_MULT --filter-mode $FILTER_MODE --rm-quantile-percentile $RM_QUANT_PC" 
+denoising="--rm-quantile-multiple $RM_QUANT_MULT --filter-mode $FILTER_MODE --rm-quantile-percentile $RM_QUANT_PC --rm-min-matches $RM_MIN_MATCHES --rm-threshold $RM_THRESHOLD" 
 filtering="--median-filter-size $MED_FILTER_SIZE --texture-smooth-size $TEXT_SMOOTH_SIZE --texture-smooth-scale $TEXT_SMOOTH_SCALE"
 
 parallel_stereo $session $IMG1_CROP $IMG2_CROP $BLACK_LEFT $BLACK_RIGHT $OUTPUT_DIR $stereo $filtering  
